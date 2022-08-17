eb0373778f1fdaab4c302d0231f8d3f5f91972ca
OscarAlex <oscar.cbin.mx@gmail.com>
2022-08-17 14:51:49 -0500

    Correcciones

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,6 @@
-
 (function() {
+
+    let item_global = '';
 
     // Variables
     var srcAlert = document.getElementById("modTarea");
@@ -13,7 +14,7 @@
     btnde = document.getElementById("btnde");
     btnModal = document.getElementById("agregar");
     btnDelete = document.getElementById("delete");
-    btnChoose = document.getElementById("escoger");
+    // btnChoose = document.getElementById("escoger");
 
     // Eventos
     btnModal.addEventListener("click", mostrar)
@@ -22,7 +23,9 @@
     btnHi.addEventListener("click", agregar)
     btnde.addEventListener("click", eliminarEscoge)
     btnDelete.addEventListener("click", limLista)
-    btnChoose.addEventListener("click", mostrarEscoger)
+    // btnChoose.addEventListener("click", mostrarEscoger)
+
+    takeLocalStorage();
 
     // Función para mostrar modal
     function mostrar() {
@@ -44,10 +47,86 @@
         srcChose.setAttribute("class", "delete oculto");
     }
 
+    // Funcion para mostrar en una ventana la tarea agregada
     function ejecutaAlerta(item) {   
         var w = window.open('','','width=300,height=100');
         w.document.write(`Se añadió la tarea '${item}' a la lista!`);
         setTimeout(function() { w.close(); }, 2000);
+    }
+
+    // Función para tomar lo de local Storage
+    function takeLocalStorage() {
+        const local = localStorage.getItem("homeworks");
+        if ( local !== null ) {
+            let local_parse = JSON.parse(local);
+            for ( let aux of local_parse ) {
+                var elemento = document.createElement("li");
+
+                contenido = document.createTextNode(aux.homework);
+                elemento.appendChild(contenido);
+                if ( aux.mark == false ) {
+                    elemento.removeAttribute("class");
+                    // saveLocalStorage(item, false);
+                } else {
+                    elemento.setAttribute("class", "hecho");
+                    // saveLocalStorage(item, true);
+                }
+                // item_global = aux.homework;
+                elemento.addEventListener("click", marcar);
+                
+                elementoIcon = document.createElement("span");
+                elementoIcon.setAttribute("class", "material-icons");
+                contenidoIcon = document.createTextNode("close");
+                elementoIcon.appendChild(contenidoIcon);
+                elementoIcon.addEventListener("click", eliminarOne);
+                // elementoIcon.addEventListener("click", alert('eliminar'))
+                elemento.appendChild(elementoIcon);
+
+                lstLista.appendChild(elemento);
+            }
+        }
+    }
+
+    //  Función para eliminar de Local Storage
+    function deleteLocalStorage(item) {
+        const local = localStorage.getItem("homeworks"); 
+
+        if ( local !== null ) {
+            array_homework =  JSON.parse(local);
+            const new_homeworks = array_homework.filter((aux) => aux.homework !== item);
+            localStorage.setItem("homeworks", JSON.stringify(new_homeworks));
+        }
+    }
+
+    // Función para guardar en LocalStorage
+    function saveLocalStorage(item, mark) {
+        const local = localStorage.getItem("homeworks");
+        let array_homework = [];
+        let obj_homework = {
+            homework: item,
+            mark: mark
+        }
+        
+        if ( !local || local == null ) {
+            array_homework.push(obj_homework);
+            localStorage.setItem("homeworks", JSON.stringify(array_homework));
+        } else {
+            array_homework =  JSON.parse(local);
+            let exist = false;
+            for ( let aux of array_homework ) {
+                if ( aux.homework === obj_homework.homework ) {
+                    aux.mark =  obj_homework.mark;
+                    exist = true;
+                }
+            }
+
+            if ( exist == true ) {
+                localStorage.setItem("homeworks", JSON.stringify(array_homework));
+            } else {
+                array_homework.push(obj_homework);
+                localStorage.setItem("homeworks", JSON.stringify(array_homework));
+            }
+        }
     }
 
     // Función para agregar una tarea nueva
@@ -62,6 +141,7 @@
 
             contenido = document.createTextNode(item);
             elemento.appendChild(contenido);
+            // item_global = item;
             elemento.addEventListener("click", marcar);
             
             elementoIcon = document.createElement("span");
@@ -72,6 +152,7 @@
             // elementoIcon.addEventListener("click", alert('eliminar'))
             elemento.appendChild(elementoIcon);
             ejecutaAlerta(item);
+            saveLocalStorage(item, false);
             
             lstLista.appendChild(elemento);
             txtTexto.setAttribute("placeholder", "Agregar una tarea a la lista");
@@ -81,15 +162,25 @@
 
     function eliminarOne() {
         // console.log(this.parentNode.remove());
+        let palabras = this.parentNode.innerHTML.split('<');
+        deleteLocalStorage(palabras[0]);
         this.parentNode.remove();
     }
 
     // Función para tachar tarea en caso de seleccionarla
     function marcar() {
-        if (this.getAttribute("class") == "hecho") {
-            this.removeAttribute("class");
-        } else {
-            this.setAttribute("class", "hecho");
+        const local = JSON.parse(localStorage.getItem("homeworks"));
+        let palabras = this.innerHTML.split('<');
+        let exist = local.filter((aux) => aux.homework === palabras[0]);
+
+        if ( exist.length > 0 ) {
+            if (this.getAttribute("class") == "hecho") {
+                this.removeAttribute("class");
+                saveLocalStorage(palabras[0], false);
+            } else {
+                this.setAttribute("class", "hecho");
+                saveLocalStorage(palabras[0], true);
+            }
         }
         // valor.parentNode.parentNode.removeChild(valor.parentNode);
         // this.classList.add("oculto");
@@ -130,3 +221,4 @@
     }
 
 }());
+
